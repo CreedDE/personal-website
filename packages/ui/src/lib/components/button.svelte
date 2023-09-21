76d8bf0d68f2628e8446868f725705322f9c5832
CreedDE <tim-holz1@web.de>
2023-09-21 19:40:30 +0200

    feat: add button
    closes #23

--- packages/ui/src/lib/components/button.svelte
+++ packages/ui/src/lib/components/button.svelte
@@ -1,9 +1,52 @@
 <script lang="ts">
+  import { Button as ButtonPrimitive } from 'bits-ui';
+  import { type VariantProps, cva } from 'class-variance-authority';
+  import { cn } from '$lib/utils.js';
+
+  const buttonVariants = cva(
+    'inline-flex items-center justify-center font-medium whitespace-nowrap m-0 rounded-lg border ease-out duration-200 shadow-sm',
+    {
+      variants: {
+        size: {
+          default: 'py-2 px-4 text-sm'
+        },
+        variant: {
+          default: 'bg-secondary/70 border-border text-foreground hover:bg-secondary',
+          primary:
+            'bg-primary/70 border-primary text-background hover:bg-primary dark:text-foreground',
+          link: 'text-foreground hover:bg-secondary border-0 shadow-none'
+        }
+      },
+      defaultVariants: {
+        variant: 'default',
+        size: 'default'
+      }
+    }
+  );
+
+  type Variant = VariantProps<typeof buttonVariants>['variant'];
+  type Size = VariantProps<typeof buttonVariants>['size'];
+
+  type $$Props = ButtonPrimitive.Props & {
+    variant?: Variant;
+    size?: Size;
+  };
+
+  type $$Events = ButtonPrimitive.Events;
+
+  let className: $$Props['class'] = undefined;
+  export let variant: $$Props['variant'] = 'default';
+  export let size: $$Props['size'] = 'default';
+  export let builders: $$Props['builders'] = [];
+  export { className as class };
 </script>
 
-<button
-  class="bg-primary text-primary-foreground rounded-lg hover:bg-primary/90 h-10 px-4 py-2"
+<ButtonPrimitive.Root
+  {builders}
+  class={cn(buttonVariants({ variant, size, className }))}
+  {...$$restProps}
   on:click
+  on:keydown
 >
   <slot />
-</button>
+</ButtonPrimitive.Root>
--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+  import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+  import Button from '$lib/components/button.svelte';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ThemeToggle />
+
+<Button>Documentation</Button>
+<Button variant="primary">Documentation</Button>
+<Button variant="link" href="/test">Documentation</Button>
