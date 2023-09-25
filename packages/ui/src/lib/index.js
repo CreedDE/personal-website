f34bd0a9653a7e24da0b762f454dd3a585b54afe
CreedDE <tim-holz1@web.de>
2023-09-25 15:42:28 +0200

    feat: adding lucide icons to both projects
    closes #36

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './components/button.svelte';
 export { default as ThemeToggle } from './components/ThemeToggle.svelte';
 
 export * as Card from './components/card/index.js';
+export * as Sheet from './components/sheet/index.js';
