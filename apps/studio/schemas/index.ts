b331fe7b35f39a1dd9f71f54b3562840e50f3478
CreedDE <tim-holz1@web.de>
2023-10-02 18:35:18 +0200

    feat(studio): add imprint singleton
    closes #55

--- apps/studio/schemas/index.ts
+++ apps/studio/schemas/index.ts
@@ -5,6 +5,17 @@ import milestone from './objects/milestone';
 import timeline from './objects/timeline';
 import pet from './pet';
 import home from './singleton/home';
+import imprint from './singleton/imprint';
 import settings from './singleton/settings';
 
-export const schemaTypes = [home, settings, duration, page, project, milestone, timeline, pet];
+export const schemaTypes = [
+  home,
+  settings,
+  imprint,
+  duration,
+  page,
+  project,
+  milestone,
+  timeline,
+  pet
+];
